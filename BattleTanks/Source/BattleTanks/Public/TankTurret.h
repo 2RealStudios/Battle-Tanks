ab27b1bfdd0d86e442cdf203aa13fe1d9e5895df
Texasjake95 <texasjake95@gmail.com>
2017-02-06 15:21:13 -0600

    43 CHALLENGE - Turret Rotaton Pt.2

--- BattleTanks/Source/BattleTanks/Public/TankTurret.h
+++ BattleTanks/Source/BattleTanks/Public/TankTurret.h
@@ -11,8 +11,14 @@ UCLASS( ClassGroup = (Custom), meta = (BlueprintSpawnableComponent), hidecategor
 class BATTLETANKS_API UTankTurret : public UStaticMeshComponent
 {
 	GENERATED_BODY()
+
+public:
 	
-	
-	
-	
+	// -1 is max downward speed, and +1 is max up movement
+	void Rotate(float RelativeSpeed);
+
+private:
+	UPROPERTY(EditAnywhere)
+	float MaxDegreesPerSecond = 25;
+
 };
