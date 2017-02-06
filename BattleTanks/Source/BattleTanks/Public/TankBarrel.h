1b401b122f11154c2910f298dde69441fbe2aae5
Texasjake95 <texasjake95@gmail.com>
2017-02-05 22:23:36 -0600

    39 Review Our Execution Flow

--- BattleTanks/Source/BattleTanks/Public/TankBarrel.h
+++ BattleTanks/Source/BattleTanks/Public/TankBarrel.h
@@ -11,7 +11,9 @@ class BATTLETANKS_API UTankBarrel : public UStaticMeshComponent
 	GENERATED_BODY()
 	
 public:
-	void Elevate(float DegreesPerSecond);
+
+	// -1 is max downward speed, and +1 is max up movement
+	void Elevate(float RelativeSpeed);
 	
 private:
 	UPROPERTY(EditAnywhere)
