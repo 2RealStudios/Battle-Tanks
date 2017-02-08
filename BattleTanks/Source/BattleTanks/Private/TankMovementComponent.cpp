53bf17368faa623d12b614229daffd8a8c3660b2
Texasjake95 <texasjake95@gmail.com>
2017-02-08 17:15:25 -0600

    58 Completing Manual Tank Movement

--- BattleTanks/Source/BattleTanks/Private/TankMovementComponent.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankMovementComponent.cpp
@@ -6,22 +6,37 @@
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
+	if (!LeftTrack || !RightTrack)
+	{
+		return;
+	}
 	//auto Time = GetWorld()->GetTimeSeconds();
 	//UE_LOG(LogTemp, Warning, TEXT("Indend move forward throw: %f"), Throw);
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
+	// TODO prevent dual controll input
+
+}
+
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	if (!LeftTrack || !RightTrack)
+	{
+		return;
+	}
+	//auto Time = GetWorld()->GetTimeSeconds();
+	//UE_LOG(LogTemp, Warning, TEXT("Indend move forward throw: %f"), Throw);
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
+	// TODO prevent dual controll input
 
 }
 
 void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-	if (!LeftTrackToSet || !RightTrackToSet)
-	{
-		return;
-	}
+	
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
-	// TODO prevent dual controll input
 
 }
 
