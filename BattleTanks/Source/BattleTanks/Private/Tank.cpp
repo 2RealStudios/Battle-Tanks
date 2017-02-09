9c9da933277b100e4ac3dc9a0f35c300a7843296
Texasjake95 <texasjake95@gmail.com>
2017-02-08 22:39:03 -0600

    70 Decoupling Your Architecture

--- BattleTanks/Source/BattleTanks/Private/Tank.cpp
+++ BattleTanks/Source/BattleTanks/Private/Tank.cpp
@@ -12,6 +12,7 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+
 	auto TankName = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%s DONKEY: Tank C++ Construct"), *TankName);
 
@@ -21,6 +22,7 @@ ATank::ATank()
 void ATank::BeginPlay()
 {
 	Super::BeginPlay(); // Needed for BP Begin Play to work
+
 	auto TankName = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%s DONKEY: Tank C++ BeginPlay"), *TankName);
 }
