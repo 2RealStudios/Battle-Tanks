33b6b02752ab98d963b31313e6d52b8f61181489
Texasjake95 <texasjake95@gmail.com>
2017-02-10 20:41:20 -0600

    95 BlueprintPure & Health Bars

--- BattleTanks/Source/BattleTanks/Public/Tank.h
+++ BattleTanks/Source/BattleTanks/Public/Tank.h
@@ -16,6 +16,10 @@ class BATTLETANKS_API ATank : public APawn
 public:
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
 
+	// Returns current health as a percentage of starting health, between 0 and 1
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetHealthPercent() const;
+
 private:
 
 	UPROPERTY(EditDefaultsOnly, Category = "Setup")
