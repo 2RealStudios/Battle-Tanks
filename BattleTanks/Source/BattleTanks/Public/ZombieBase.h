a1b60411310d13d7fb43347eaa98876ea732aad8
Texasjake95 <texasjake95@gmail.com>
2017-03-01 23:23:27 -0600

    Revert "Revert "First Round of Zombies basic movement and Engine Mesh""
    This reverts commit 43929bf678df6cbaa2e499039445fcc673faa4ec.

---
+++ BattleTanks/Source/BattleTanks/Public/ZombieBase.h
@@ -0,0 +1,30 @@
+
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "ZombieBase.generated.h"
+
+UCLASS()
+class BATTLETANKS_API AZombieBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AZombieBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	
+	
+};
